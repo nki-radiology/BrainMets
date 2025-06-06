d0a7a2639c2eedee5d86db8254bdadf4d538cd8f
JeroenBertelsRobovision <jeroen.bertels@robovision.eu>
2025-06-06 16:49:41 +0200

    First draft.

---
+++ postprocessing/postprocessing.py
@@ -0,0 +1,16 @@
+class Postprocessing(object):
+    @staticmethod
+    def run_postprocessing(inference_dir, postprocessing_dir):
+        return
+
+
+if __name__ == "__main__":
+    import argparse
+
+    argParser = argparse.ArgumentParser()
+    argParser.add_argument("--inference-dir", help="Directory in which the predictions are stored.", required=True)
+    argParser.add_argument(
+        "--postprocessing-dir", help="Directory in which the postprocessing files will be stored.", required=True
+    )
+    args = argParser.parse_args()
+    Postprocessing.run_postprocessing(args.inference_dir, args.postprocessing_dir)
---
+++ preprocessing/preprocessing.py
@@ -0,0 +1,42 @@
+import os
+import dicom2nifti
+from HD_BET.run import run_hd_bet
+
+
+class Preprocessing(object):
+    @staticmethod
+    def series_to_nifti(dicom_series_directory, nifti_file_path):
+        dicom2nifti.dicom_series_to_nifti(dicom_series_directory, nifti_file_path, reorient_nifti=False)
+
+    @staticmethod
+    def apply_brain_mask(nifti_file_path, masked_nifti_file_path):
+        run_hd_bet(nifti_file_path, masked_nifti_file_path, bet=True, keep_mask=False)
+
+    @staticmethod
+    def run_preprocessing(data_dir, preprocessing_dir):
+        scan_names = sorted(os.listdir(data_dir))
+        for i, scan_name in enumerate(scan_names):
+            print(f"Preprocessing {i + 1} / {len(scan_names)}")
+            scan_dir_in = os.path.join(data_dir, scan_name)
+            scan_dir_out = os.path.join(preprocessing_dir, scan_name)
+            os.makedirs(scan_dir_out, exist_ok=True)
+            scan_nii_path = os.path.join(scan_dir_out, "mri.nii.gz")
+            Preprocessing.series_to_nifti(scan_dir_in, scan_nii_path)
+            masked_scan_nii_path = os.path.join(scan_dir_out, "brain_masked_mri.nii.gz")
+            Preprocessing.apply_brain_mask(scan_nii_path, masked_scan_nii_path)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    argParser = argparse.ArgumentParser()
+    argParser.add_argument(
+        "--data-dir",
+        help="Directory in which the raw DICOM data is stored in the correct folder structure.",
+        required=True,
+    )
+    argParser.add_argument(
+        "--preprocessing-dir", help="Directory in which the preprocessed Nifti files will be stored.", required=True
+    )
+    args = argParser.parse_args()
+    Preprocessing.run_preprocessing(args.data_dir, args.preprocessing_dir)
